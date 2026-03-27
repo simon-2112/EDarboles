0b9866e8ceb2c0d4dc22bd4b6c2f16f6706899c9
javier <alejandmosquera@gmail.com>
2026-03-27 17:41:57 -0500

    Information was added to the (about) section

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,3 +1,12 @@
 document.getElementById('btn-enter').addEventListener('click', () => {
        window.location.href = 'app.html';
    });
+
+document.getElementById('btn-about').addEventListener('click', () => {
+       window.location.href = 'about.html';
+   });
+
+document.getElementById('btn-documentation').addEventListener('click', () => {
+       // Funcionalidad de documentación a implementar
+       alert('Sección de documentación en desarrollo');
+   });
