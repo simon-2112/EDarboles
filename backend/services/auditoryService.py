61ed2daf79ffc37c7be8ca1e932852d5dd0da1ea
nico <nicolasramireza01@gmail.com>
2026-03-28 00:26:13 -0500

    fixed error auditory

--- backend/services/auditoryService.py
+++ backend/services/auditoryService.py
@@ -23,28 +23,20 @@ class AuditoryService:
         if node is None:
             return None, []
         
-        avl = self.treeService.avl
 
         leftReport, leftInconsistent = self._auditNode(node.getLeftChild())
         rightReport, rightInconsistent = self._auditNode(node.getRightChild())
 
-        # Alturas de subárboles
-        # left_height = leftReport["altura"] if leftReport else 0
-        # right_height = rightReport["altura"] if rightReport else 0
+        left_height = leftReport["altura"] if leftReport else 0
+        right_height = rightReport["altura"] if rightReport else 0
 
-        # Factor de balance
-        balance = avl.balance_factor(node)
-        # balance = left_height - right_height
+        balance = left_height - right_height
+        height = 1 + max(left_height, right_height)
 
-        # Altura real
-        height = avl.getHeight(node)
-        # height = 1 + max(left_height, right_height)
-
-        # Nodo consistente si |factor| <= 1
         isConsistent = abs(balance) <= 1
 
-        # Crear reporte de este nodo
         flight = node.getValue()
+        
         nodeReport = {
             "codigo": flight.getIdFlight(),
             "altura": height,
